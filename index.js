1921db8268e9c74543a072f23f5a57de3e6112fc
Sarthak gupta <guptasarthak03@gmail.com>
2022-08-23 18:33:44 +0530

    Feat: Elements with Styling

---
+++ index.js
@@ -0,0 +1,7 @@
+const input = document.querySelector('.translation-input');
+const btnTranslate = document.querySelector('.btn-translate');
+const output = document.querySelector('.translation-output');
+
+btnTranslate.addEventListener(btnTranslate, () => {
+  console.log(input);
+});
