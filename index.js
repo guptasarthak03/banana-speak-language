bc193592db4eabeb906bc29cb6440396e7bb35bb
Sarthak gupta <guptasarthak03@gmail.com>
2022-08-23 19:09:18 +0530

    Feat: Dom manipulation

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-const input = document.querySelector('.translation-input');
+const inputDOM = document.querySelector('.translation-input');
 const btnTranslate = document.querySelector('.btn-translate');
-const output = document.querySelector('.translation-output');
+const outputDOM = document.querySelector('.translation-output');
 
-btnTranslate.addEventListener(btnTranslate, () => {
-  console.log(input);
+// const fetchUrl = 'https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json';  // Mock Server
+const fetchUrl = 'https://api.funtranslations.com/translate/minion.json'; // Translate App
+
+const constructUrl = input => {
+  const encodedUrl = encodeURI(fetchUrl);
+  return `${encodedUrl}?text=${input}`;
+};
+
+btnTranslate.addEventListener('click', () => {
+  console.log(inputDOM.value);
+  const input = inputDOM.value;
+
+  fetch(constructUrl(input))
+    .then(res => res.json())
+    .then(data => {
+      outputDOM.value = data.contents.translated;
+    })
+    .catch(err => console.log('OOps! You ran into some error.', err));
 });
